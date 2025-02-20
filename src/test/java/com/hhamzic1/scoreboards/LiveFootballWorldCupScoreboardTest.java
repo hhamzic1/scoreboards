3c6068c3ad4e3696b46e7a72ca5b8d9705baf672
hhamzic1 <hamzichuso@gmail.com>
2025-02-20 18:29:08 +0100

    - Removing licence (redundant) - Enriching .gitignore - Setting up maven - Setting up project structure (ready for TDD)

---
+++ src/test/java/com/hhamzic1/scoreboards/LiveFootballWorldCupScoreboardTest.java
@@ -0,0 +1,11 @@
+package com.hhamzic1.scoreboards;
+
+import org.junit.jupiter.api.Test;
+
+public class LiveFootballWorldCupScoreboardTest {
+
+    @Test
+    public void test() {
+        System.out.println("Hello world");
+    }
+}
