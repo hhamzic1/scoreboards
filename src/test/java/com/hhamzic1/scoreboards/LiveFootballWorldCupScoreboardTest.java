0d6080eccca1b8fbd12fb3c05a66a6a2556562f5
hhamzic1 <hamzichuso@gmail.com>
2025-02-20 18:45:15 +0100

    RED: Defining how the library should look like and how to interact with it. Created two simple tests for 'startMatch' method.

--- src/test/java/com/hhamzic1/scoreboards/LiveFootballWorldCupScoreboardTest.java
+++ src/test/java/com/hhamzic1/scoreboards/LiveFootballWorldCupScoreboardTest.java
@@ -1,11 +1,36 @@
 package com.hhamzic1.scoreboards;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LiveFootballWorldCupScoreboardTest {
 
+    private Scoreboard scoreboard;
+
+    @BeforeEach
+    public void setup() {
+        scoreboard = ScoreboardFactory.createScoreboard(SportType.FOOTBALL);
+    }
+
     @Test
-    public void test() {
-        System.out.println("Hello world");
+    public void givenDifferentTeams_whenStartingMatch_thenStartMatchSuccessfully() {
+        var spain = new Team("Spain");
+        var france = new Team("France");
+
+        var match = scoreboard.startMatch(spain, france);
+
+        assertEquals(match.homeTeam().id(), spain.id());
+        assertEquals(match.awayTeam().id(), france.id());
+        assertEquals(0, match.score().homeTeamScore());
+        assertEquals(0, match.score().awayTeamScore());
+    }
+
+    @Test
+    public void givenNotValidTeams_whenStartingMatch_thenThrow() {
+        var spain = new Team("Spain");
+
+        assertThrows(ScoreboardException.class, () -> scoreboard.startMatch(spain, null));
+        assertThrows(ScoreboardException.class, () -> scoreboard.startMatch(null, spain));
+        assertThrows(ScoreboardException.class, () -> scoreboard.startMatch(spain, spain));
     }
 }
